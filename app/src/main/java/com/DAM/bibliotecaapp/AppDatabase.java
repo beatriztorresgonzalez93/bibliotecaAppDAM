197b571ac865e8b6e4d80c6ebbafddcdf43839e6
beatriz torres <beatriztorres@gmail.com>
2026-02-11 22:46:16 +0100

    base de datos creadas usuarios bibliotecario y libros

--- app/src/main/java/com/DAM/bibliotecaapp/AppDatabase.java
+++ app/src/main/java/com/DAM/bibliotecaapp/AppDatabase.java
@@ -6,7 +6,7 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {Usuario.class, Libro.class, Ejemplar.class, Prestamo.class}, version = 6)
+@Database(entities = {Usuario.class, Libro.class, Ejemplar.class, Prestamo.class}, version = 7)
 public abstract class AppDatabase extends RoomDatabase {
 
     public abstract UsuarioDao usuarioDao();
