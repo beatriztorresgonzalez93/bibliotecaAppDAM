f748c9d22d8cad12d88ca22d3c1f20d448cea2bc
beatriz torres <beatriztorres@gmail.com>
2026-05-07 13:42:11 +0200

    actualizacion historyseed.json y gradle para que compile

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,7 +7,9 @@ pluginManagement {
                 includeGroupByRegex("androidx.*")
             }
         }
-        mavenCentral()
+        maven {
+            url = uri("https://maven.aliyun.com/repository/central")
+        }
         gradlePluginPortal()
     }
 }
@@ -15,7 +17,12 @@ dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
-        mavenCentral()
+        maven {
+            url = uri("https://maven.aliyun.com/repository/central")
+        }
+        maven {
+            url = uri("https://maven.aliyun.com/repository/public")
+        }
         maven {
             url = uri("https://jitpack.io")
         }
